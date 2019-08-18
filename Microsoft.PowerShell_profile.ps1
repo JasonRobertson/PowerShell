c5dc39efdb5237dbf9c8c4a9e94d1e4fb787126d
JasonRobertson <Jason@mxcloud.services>
2019-08-18 14:29:13 -0700

    Added the functions into the PowerShell profile.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,54 @@
+Set-PSReadlineOption -BellStyle None
+
+function Connect-ExchangeOnline     {
+    [CmdletBinding()]
+    [Alias("Connect-EXO")]
+    param (
+    )
+
+    begin {
+        #Import the module, requires that you are administrator and are able to run the script
+        Try{
+            Import-Module -Name (Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse).Fullname[-1] -ErrorAction Stop -Verbose
+        }
+        Catch{
+            Write-Warning $_.Exception.Message
+        }
+    }
+    process {
+        #Connect specifying username, if you already have authenticated to another moduel, you actually do not have to authenticate
+        Try {
+            Connect-ExoPSSession
+        }
+        Catch{
+            Write-Warning $_.Exception.Message
+        }
+    }
+    end {
+
+        Remove-Module CreateExoPSSession
+    }
+}
+function Connect-SecurityAndCompliance {
+    [CmdletBinding()]
+    [Alias("Connect-IPP")]
+    param (
+    )
+    begin {
+        #Import the module, requires that you are administrator and are able to run the script
+        Import-Module (Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse).Fullname[-1]
+    }
+    process {
+        #Connect specifying username, if you already have authenticated to another moduel, you actually do not have to authenticate
+        Try {
+            Connect-IPPSSession
+        }
+        Catch{
+            Write-Warning $_.Exception.Message
+        }
+    }
+    end {
+
+        Remove-Module CreateExoPSSession
+    }
+}
